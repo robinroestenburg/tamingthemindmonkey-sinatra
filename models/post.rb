fac44c2f32cca206312d77975361c12faa6ff093
Robin Roestenburg <robinroestenburg@gmail.com>
2012-03-08 17:07:32 +0100

    Cached the result of the all_posts method for performance.

--- models/post.rb
+++ models/post.rb
@@ -8,6 +8,68 @@ class Post
                 :filename
 
   POSTS_DIR = 'posts'
+  
+  class << self
+
+    def all_posts
+      @@all_posts ||=
+        Dir.new(POSTS_DIR).
+          select { |file_name| file_name != '.' &&  file_name != '..' }.
+          collect { |file_name| build("#{POSTS_DIR}/#{file_name}") }.
+          select { |post| post.published_at }.
+          sort { |post, other| other.published_at <=> post.published_at }
+    end
+
+
+    def build(file_name)
+      preamble, content = parse_file(file_name)
+
+      post              = new
+      post.filename     = file_name
+      post.content      = content
+      post.title        = preamble['title']
+      post.author       = preamble['author']
+      post.published_at = Date.parse(preamble['published_at']) if preamble['published_at']
+      post.tags         = parse_tag_list(preamble['tags'])
+      post
+    end
+
+    def find_by_name(file_name)
+      raise(ArgumentError, 'File does not exist!') unless File.file? file_name
+
+      build(file_name)
+    end
+
+    def find_by_year(year)
+      grouped_by_year_and_month[year]
+    end
+
+    def find_by_month(year, month)
+      grouped_by_year_and_month[year][month]
+    end
+
+    def grouped_by_year_and_month
+      Hash[
+        all_posts.
+          group_by { |post| post.published_at.year }.
+          collect do |year, posts_by_year|
+            [ year, posts_by_year.group_by { |post| post.published_at.month } ]
+          end]
+    end
+
+    def parse_tag_list(tags)
+      return [] unless tags.is_a? String
+      return [] if tags.empty?
+
+      tags.split.uniq
+    end
+
+    private
+
+    def parse_file(file_name)
+      Preamble.load(file_name)
+    end
+  end
 
   def initialize
     @tags = []
@@ -24,6 +86,7 @@ class Post
       end
     end
   end
+
   def permalink
     match_data = filename.match(/#{POSTS_DIR}\/(\d+)-(\d+)-(\d+)-(.+)\.markdown/)
     "#{match_data[1]}/#{match_data[2]}/#{match_data[3]}/#{match_data[4]}"
@@ -36,6 +99,8 @@ class Post
   def next_post
     index = post_position
     Post.all_posts.to_a.fetch(index - 1) unless last_post? index
+  rescue IndexError
+    false
   end
 
   def has_previous?
@@ -50,65 +115,10 @@ class Post
   end
 
   def ==(other)
-    self.filename == other.filename
-  end
-
-  def self.build(file_name)
-    preamble, content = parse_file(file_name)
-
-    post = Post.new
-    post.filename     = file_name
-    post.content      = content
-    post.title        = preamble['title']
-    post.author       = preamble['author']
-    post.published_at = Date.parse(preamble['published_at']) if preamble['published_at']
-    post.tags         = Post.parse_tag_list(preamble['tags'])
-    post
-  end
-
-  def self.find_by_name(file_name)
-    raise(ArgumentError, 'File does not exist!') unless File.file? file_name
-
-    Post.build(file_name)
-  end
-
-  def self.find_by_year(year)
-    grouped_by_year_and_month[year]
-  end
-
-  def self.find_by_month(year, month)
-    grouped_by_year_and_month[year][month]
-  end
-
-  def self.grouped_by_year_and_month
-    Hash[
-      all_posts.
-        group_by { |post| post.published_at.year }.
-        collect do |year, posts_by_year|
-          [ year, posts_by_year.group_by { |post| post.published_at.month } ]
-        end]
-  end
-
-  def self.all_posts
-    Dir.new(POSTS_DIR).
-      select { |file_name| file_name != '.' &&  file_name != '..' }.
-      collect { |file_name| Post.build("#{POSTS_DIR}/#{file_name}") }.
-      select { |post| post.published_at }.
-      sort { |post, other| other.published_at <=> post.published_at }
-  end
-
-  def self.parse_tag_list(tags)
-    return [] unless tags.is_a? String
-    return [] if tags.empty?
-
-    tags.split.uniq
+    filename == other.filename
   end
 
   private
-
-  def self.parse_file(file_name)
-    Preamble.load(file_name)
-  end
 
   def post_position
     Post.all_posts.find_index(Post.build(filename))
